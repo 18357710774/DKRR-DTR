b4fdfa74eefc9ec5586448a3e7441e5b12ff2a69
DiWang <60568287+18357710774@users.noreply.github.com>
2023-03-30 18:32:45 +0800

    Update fcnet_testing.m

--- DKRRDTR/tools/fcnet_testing.m
+++ DKRRDTR/tools/fcnet_testing.m
@@ -1,7 +1,7 @@
 function estimated = fcnet_testing(algo, X)
 n = size(X,1);
 if isempty(algo.fc_model) || n==0
-    estimated = -ones(n,1)*inf;
+    estimated = zeros(n,1);
 else
     estimated = predict(algo.fc_model, X);
 end
